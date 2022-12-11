2b8e77ee1025d6ac11ab3a6c05c7287462549ab9
calvin <ckodalo@gmail.com>
2022-12-11 14:39:21 +0300

    added middleware configurations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     
     #create sesssion after auth
     def create
-        user = User.find_by(username: params[:name])
+        user = User.find_by(name: params[:name])
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
             render json: user, status: :created
